5f65bf1e0f5d872a7939ff2456b030c6239224c8
kaiannovais <kaiandiasnovais@gmail.com>
2026-04-28 12:44:53 -0300

    feat(routing): add Routes and RouteNames constants

---
+++ lib/routing/routes.dart
@@ -0,0 +1,11 @@
+abstract class Routes {
+  static const splash = '/splash';
+  static const onboarding = '/onboarding';
+  static const home = '/home';
+}
+
+abstract class RouteNames {
+  static const splash = 'splash';
+  static const onboarding = 'onboarding';
+  static const home = 'home';
+}
